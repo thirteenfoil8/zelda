c15227c0d65c86407895c16bcc635e55ec8e52ba
thirteenfoil8 <florian.genilloud@gmail.com>
2023-01-20 21:37:15 +0100

    start tile logic from level

--- src/zelda/level/level.py
+++ src/zelda/level/level.py
@@ -3,6 +3,7 @@ from zelda.config.settings import WORLD_MAP, TILESIZE
 from zelda.level.tile import Tile
 from zelda.player.player import Player
 from zelda.camera.camera import YSortCameraGroup
+from zelda.utils.support import import_csv_layout
 
 
 class Level:
@@ -19,17 +20,25 @@ class Level:
         self.create_map()
 
     def create_map(self):
-        # for row_index, row in enumerate(WORLD_MAP):
-        #     for col_index, col in enumerate(row):
-        #         x = col_index * TILESIZE
-        #         y = row_index * TILESIZE
+        layouts = {
+            'boundary': import_csv_layout('src/zelda/assets/map/map_FloorBlocks.csv')
+        }
+        for style, layout in layouts.items():
+            for row_index, row in enumerate(WORLD_MAP):
+                for col_index, col in enumerate(row):
+                    x = col_index * TILESIZE
+                    y = row_index * TILESIZE
+                if style == "boundary":
+                    Tile(pos=(x, y), groups=[
+                         self.visible_sprites, self.obstacles_sprites],sprite_type='invisible')
         #         if col == "x":
         #             Tile(pos=(x, y), groups=[
         #                  self.visible_sprites, self.obstacles_sprites])
         #         if col == "p":
         #             self.player = Player(pos=(x, y), groups=[self.visible_sprites],
         #                                  obstacles_sprites=self.obstacles_sprites)
-        self.player = Player(pos=(2000, 1430), groups=[self.visible_sprites],obstacles_sprites=self.obstacles_sprites)
+        self.player = Player(pos=(2000, 1430), groups=[
+                             self.visible_sprites], obstacles_sprites=self.obstacles_sprites)
 
     def run(self):
         # Update and draw game
